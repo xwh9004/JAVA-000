df30991796876320229da19b469822abdb4842b3
xuwuhui <wuhui.xu@newtouch.cn>
2020-12-02 17:03:40 +0800

    shardingsphere jdbc

--- Week_07/order-service/src/main/java/com/example/order/config/DataSourceConfig.java
+++ Week_07/order-service/src/main/java/com/example/order/config/DataSourceConfig.java
@@ -1,23 +1,13 @@
 package com.example.order.config;
 
-import com.example.order.db.DynamicRoutingDataSource;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * <p><b>Description:</b>
@@ -28,10 +18,10 @@ import java.util.Map;
  * @version V0.1
  * @classNmae DataSourceConfig
  */
-@Configuration
-public class DataSourceConfig implements ApplicationContextAware {
 
-    private ApplicationContext context;
+@Configuration
+public class DataSourceConfig {
+
 
     @Bean("primaryDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.primary")
@@ -53,36 +43,11 @@ public class DataSourceConfig implements ApplicationContextAware {
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate(){
-
+    public JdbcTemplate jdbcTemplate(@Qualifier("shardingSphereDataSource") DataSource dataSource){
         //先配置默认数据源
-        return new JdbcTemplate(primaryDataSource());
-    }
-//    @Bean("primaryJdbcTemplate")
-//    public JdbcTemplate primaryJdbcTemplate(@Qualifier("primaryDataSource") DataSource dataSource){
-//        return new JdbcTemplate(dataSource);
-//    }
-//
-//    @Bean("replicationJdbcTemplate")
-//    public JdbcTemplate replicationJdbcTemplate(@Qualifier("replicationDataSource") DataSource dataSource){
-//        return new JdbcTemplate(dataSource);
-//    }
-    @Bean
-    public DynamicRoutingDataSource dynamicRoutingDataSource(){
-        DynamicRoutingDataSource routingDataSource = new DynamicRoutingDataSource();
-
-        Map<String, DataSource> dataSources = context.getBeansOfType(DataSource.class);
-
-        Map<Object, Object> routingDataSources =new HashMap<>();
-
-        routingDataSources.putAll(dataSources);
-        routingDataSource.setTargetDataSources(routingDataSources);
-        routingDataSource.afterPropertiesSet();
-        return routingDataSource;
+        return new JdbcTemplate(dataSource);
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.context = applicationContext;
-    }
+
+
 }
