c0970a09e03b1e61fe13fa15633f6871b8a24f86
xuwuhui <wuhui.xu@newtouch.cn>
2020-12-17 10:18:53 +0800

    尝试将服务端写死查找接口实现类变成泛型和反射

---
+++ Week_09/rpc01/rpcfx-demo-provider/src/main/java/io/kimmking/rpcfx/demo/provider/DemoResolver.java
@@ -0,0 +1,28 @@
+package io.kimmking.rpcfx.demo.provider;
+
+import io.kimmking.rpcfx.api.RpcfxResolver;
+import io.kimmking.rpcfx.demo.api.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class DemoResolver implements RpcfxResolver, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public <T> T resolve(String serviceClass) throws ClassNotFoundException {
+        try {
+            Class<?> cls = Class.forName(serviceClass);
+            return (T)this.applicationContext.getBean(cls);
+        } catch (ClassNotFoundException e) {
+            throw e;
+        }
+
+
+    }
+}
